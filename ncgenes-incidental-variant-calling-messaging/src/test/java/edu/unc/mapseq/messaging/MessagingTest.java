2fa12375bbc7af13668d2e9ee10ab2f54db09057
Jason Reilly <jdr0887@gmail.com>
2014-12-01 11:19:37 -0500

    posterity commit

--- ncgenes-incidental-variant-calling-messaging/src/test/java/edu/unc/mapseq/messaging/MessagingTest.java
+++ ncgenes-incidental-variant-calling-messaging/src/test/java/edu/unc/mapseq/messaging/MessagingTest.java
@@ -14,8 +14,10 @@ public class MessagingTest {
 
     @Test
     public void testQueue() {
+        // String host = "localhost";
+        String host = "152.19.198.146";
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(String.format("nio://%s:61616",
-                "localhost"));
+                host));
         Connection connection = null;
         Session session = null;
         try {
@@ -24,10 +26,15 @@ public class MessagingTest {
             Destination destination = session.createQueue("queue/ncgenes.incidental.variantcalling");
             MessageProducer producer = session.createProducer(destination);
             producer.setDeliveryMode(DeliveryMode.PERSISTENT);
-            String format = "{\"entities\":[{\"entityType\":\"Sample\",\"id\":\"%d\"},{\"entityType\":\"WorkflowRun\",\"name\":\"test-%d\"}]}";
-            // replace 123123 with something valid
-            producer.send(session.createTextMessage(String.format(format, System.getProperty("user.name"), 123123,
-                    123123)));
+            // String format =
+            // "{\"entities\":[{\"entityType\":\"Sample\",\"id\":\"%d\"},{\"entityType\":\"WorkflowRun\",\"name\":\"test-%d\"}]}";
+            // producer.send(session.createTextMessage(String.format(format, System.getProperty("user.name"), 123123,
+            // 123123)));
+
+            String format = "{\"entities\":[{\"entityType\":\"Sample\",\"id\":\"%d\"},{\"entityType\":\"WorkflowRun\",\"name\":\"%s\",\"attributes\":[{\"name\":\"version\",\"value\":\"%d\"},{\"name\":\"incidental\",\"value\":\"%d\"}]}]}";
+            String message = String.format(format, 1940353, "NCG_00609_V13_Incidental9_41926.57625135", 13, 9);
+            System.out.println(message);
+            //producer.send(session.createTextMessage(message));
         } catch (JMSException e) {
             e.printStackTrace();
         } finally {
