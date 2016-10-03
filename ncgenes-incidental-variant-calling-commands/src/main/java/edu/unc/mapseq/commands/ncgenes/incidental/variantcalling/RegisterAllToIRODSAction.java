2dd91a561bae3de905176a4dcd493aa62ee5616c
Jason Reilly <jdr0887@gmail.com>
2016-10-03 14:50:54 -0400

    more better logging

--- ncgenes-incidental-variant-calling-commands/src/main/java/edu/unc/mapseq/commands/ncgenes/incidental/variantcalling/RegisterAllToIRODSAction.java
+++ ncgenes-incidental-variant-calling-commands/src/main/java/edu/unc/mapseq/commands/ncgenes/incidental/variantcalling/RegisterAllToIRODSAction.java
@@ -35,24 +35,32 @@ public class RegisterAllToIRODSAction implements Action {
             ExecutorService es = Executors.newFixedThreadPool(2);
             List<Workflow> foundWorkflows = maPSeqDAOBeanService.getWorkflowDAO().findByName("NCGenesIncidentalVariantCalling");
 
-            if (CollectionUtils.isNotEmpty(foundWorkflows)) {
-                for (Workflow workflow : foundWorkflows) {
-                    List<WorkflowRunAttempt> workflowRunAttemptList = maPSeqDAOBeanService.getWorkflowRunAttemptDAO()
-                            .findByWorkflowId(workflow.getId());
+            if (CollectionUtils.isEmpty(foundWorkflows)) {
+                logger.warn("No workflows found...looking for NCGenesBaseline");
+                return null;
+            }
 
-                    if (CollectionUtils.isNotEmpty(workflowRunAttemptList)) {
-                        for (WorkflowRunAttempt workflowRunAttempt : workflowRunAttemptList) {
-                            if (!workflowRunAttempt.getStatus().equals(WorkflowRunAttemptStatusType.DONE)) {
-                                continue;
-                            }
-                            es.submit(new RegisterToIRODSRunnable(maPSeqDAOBeanService, workflowRunAttempt));
-                        }
-                    }
+            for (Workflow workflow : foundWorkflows) {
+                List<WorkflowRunAttempt> workflowRunAttemptList = maPSeqDAOBeanService.getWorkflowRunAttemptDAO()
+                        .findByWorkflowId(workflow.getId());
+
+                if (CollectionUtils.isEmpty(workflowRunAttemptList)) {
+                    logger.warn("No WorkflowRunAttempts found...for NCGenesIncidentalVariantCalling");
+                    return null;
                 }
 
+                for (WorkflowRunAttempt workflowRunAttempt : workflowRunAttemptList) {
+                    if (!workflowRunAttempt.getStatus().equals(WorkflowRunAttemptStatusType.DONE)) {
+                        continue;
+                    }
+                    es.submit(new RegisterToIRODSRunnable(maPSeqDAOBeanService, workflowRunAttempt));
+                }
             }
+
             es.shutdown();
-        } catch (MaPSeqDAOException e) {
+        } catch (
+
+        MaPSeqDAOException e) {
             e.printStackTrace();
         }
         return null;
